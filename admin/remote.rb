fd5292ca6d62aa6cf768111e22befa8a1cf3874e
jvsc1 <85041826+jvsc1@users.noreply.github.com>
2021-06-15 22:57:56 +0300

    Delete admin directory

--- admin/remote.rb
+++
@@ -1,8 +0,0 @@
-map = {
-  "Ege" => "jvsc1"
-}
-user = IO.read( ".git/config" )[ /remote "origin".+?(Ege|jvsc1)/m , 1]
-
-other_user = map[user]
-`git remote add #{other_user} git://github.com/#{other_user}/moonlight.git`
-`git fetch #{other_user}`
