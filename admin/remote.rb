5e726b633fcd59723bdf27338a167ff22c01be52
jvsc1 <85041826+jvsc1@users.noreply.github.com>
2021-06-15 22:54:50 +0300

    Update remote.rb

--- admin/remote.rb
+++ admin/remote.rb
@@ -4,5 +4,5 @@ map = {
 user = IO.read( ".git/config" )[ /remote "origin".+?(Ege|jvsc1)/m , 1]
 
 other_user = map[user]
-`git remote add #{other_user} git://github.com/#{other_user}/mab.git`
+`git remote add #{other_user} git://github.com/#{other_user}/moonlight.git`
 `git fetch #{other_user}`
